15760093692baedbd8bdc215e125629e3070e0a6
jasmin <jasmin.jaramillo@udea.edu.co>
2017-03-05 19:11:58 -0500

    primer commint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+export class Heroe {
+  id:number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+//templateUrl: './app.component.html',
+template:  `
+<h1>{{titulo}}</h1>
+<h2>{{heroe.nombre}} details!</h2>
+<div><label>id:</label> {{heroe.id}}</div>
+<div>
+<label>nombre: </label>
+<input [(ngModel)]="heroe.nombre" placeholder= "nombre">
+</div>
+ `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  titulo = 'LISTA DE HEROES';
+  heroe: Heroe = {
+    id: 1,
+    nombre: 'Superman'
+};
 }
+
+
